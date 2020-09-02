24d859ee0407452c268c9eda87c89b75975d14f0
Anna Stanek <aniastanekk@gmail.com>
2020-09-02 11:56:56 +0200

    Issue#2

--- js/main.js
+++ js/main.js
@@ -2,6 +2,7 @@ const form = document.forms["peopleForm"];
 const nameValue = form["name"];
 const cityValue = form["city"];
 const codeValue = form["post-code"];
+const emailValue = form["email"];
 const feedback = document.querySelector(".results");
 
 function displayCurrentState() {
@@ -16,7 +17,12 @@ form.addEventListener("submit", function (event) {
     codeValue.value.length === 6;
 
   if (submitCondition) {
-    console.log(nameValue.value, cityValue.value, codeValue.value);
+    console.log(
+      nameValue.value,
+      cityValue.value,
+      codeValue.value,
+      emailValue.value
+    );
   } else {
     alert(`Uzupełnij forularz`);
   }
@@ -24,31 +30,50 @@ form.addEventListener("submit", function (event) {
 
 // First Method
 
-// nameValue.addEventListener("input", function () {
-//   if (nameValue.value.length < 3) {
-//     console.log(`Pole niepoprawne`);
-//     nameValue.classList.add("red");
-//   } else {
-//     console.log(`Pole poprawne`);
-//     nameValue.classList.remove("red");
-//   }
-// });
-// cityValue.addEventListener("input", function () {
-//   if (cityValue.value.length < 3) {
-//     console.log(`Pole niepoprawne`);
-//   } else {
-//     console.log(`Pole poprawne`);
-//   }
-// });
+nameValue.addEventListener("input", function () {
+  if (nameValue.value.length < 3) {
+    console.log(`Pole niepoprawne`);
+    nameValue.classList.add("red");
+  } else {
+    console.log(`Pole poprawne`);
+    nameValue.classList.remove("red");
+  }
+});
+cityValue.addEventListener("input", function () {
+  if (cityValue.value.length < 3) {
+    console.log(`Pole niepoprawne`);
+    cityValue.classList.add("red");
+  } else {
+    console.log(`Pole poprawne`);
+    cityValue.classList.remove("red");
+  }
+});
 
-// const regExPostCode = /\d{2}-\d{3}/; //dopisać do poniższego kodu
-// codeValue.addEventListener("input", function () {
-//   if (codeValue.value.length != 6  ) { //dodać (codeValue.value.indexOf('-')!= 2)
-//     console.log(`Pole niepoprawne`);
-//   } else {
-//     console.log(`Pole poprawne`);
-//   }
-// });
+const regExPostCode = /\d{2}-\d{3}/;
+
+codeValue.addEventListener("input", function () {
+  if (!regExPostCode.test(codeValue.value)) {
+    //dodać (codeValue.value.indexOf('-')!= 2)
+    console.log(`Pole niepoprawne`);
+    codeValue.classList.add("red");
+  } else {
+    console.log(`Pole poprawne`);
+    codeValue.classList.remove("red");
+  }
+});
+
+const regExEmail = /^\S+@\S+\.\S+$/;
+
+emailValue.addEventListener("input", function () {
+  if (!regExEmail.test(emailValue.value)) {
+    //dodać (codeValue.value.indexOf('-')!= 2)
+    console.log(`Pole niepoprawne`);
+    emailValue.classList.add("red");
+  } else {
+    console.log(`Pole poprawne`);
+    emailValue.classList.remove("red");
+  }
+});
 
 // Second method
 // for (let i = 0; i < 3; i++) {
@@ -63,21 +88,21 @@ form.addEventListener("submit", function (event) {
 //   });
 // }
 
-//Third Method
-const validateCondition = {
-  0: (value) => value > 3,
-  1: (value) => value > 3,
-  2: (value) => value === 6,
-};
+// //Third Method
+// const validateCondition = {
+//   0: (value) => value > 3,
+//   1: (value) => value > 3,
+//   2: (value) => value === 6,
+// };
 
-for (let i = 0; i < 3; i++) {
-  form[i].addEventListener("input", function () {
-    if (validateCondition[i](form[i].value.length)) {
-      console.log(`Pole poprawne`);
-      form[i].classList.remove("red");
-    } else {
-      console.log(`Pole niepoprawne`);
-      form[i].classList.add("red");
-    }
-  });
-}
+// for (let i = 0; i < 3; i++) {
+//   form[i].addEventListener("input", function () {
+//     if (validateCondition[i](form[i].value.length)) {
+//       console.log(`Pole poprawne`);
+//       form[i].classList.remove("red");
+//     } else {
+//       console.log(`Pole niepoprawne`);
+//       form[i].classList.add("red");
+//     }
+//   });
+// }
