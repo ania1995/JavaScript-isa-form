7faa7e78f7e0a525f2abf44cd9a8af5deeb23fbc
Anna Stanek <aniastanekk@gmail.com>
2020-09-02 12:09:10 +0200

    Solution for Third Method

--- js/main.js
+++ js/main.js
@@ -4,6 +4,8 @@ const cityValue = form["city"];
 const codeValue = form["post-code"];
 const emailValue = form["email"];
 const feedback = document.querySelector(".results");
+const regExPostCode = /\d{2}-\d{3}/;
+const regExEmail = /^\S+@\S+\.\S+$/;
 
 function displayCurrentState() {
   console.log(`aktualny stan formularza ${Input.value}`);
@@ -28,52 +30,47 @@ form.addEventListener("submit", function (event) {
   }
 });
 
-// First Method
+// // First Method
+// nameValue.addEventListener("input", function () {
+//   if (nameValue.value.length < 3) {
+//     console.log(`Pole niepoprawne`);
+//     nameValue.classList.add("red");
+//   } else {
+//     console.log(`Pole poprawne`);
+//     nameValue.classList.remove("red");
+//   }
+// });
+// cityValue.addEventListener("input", function () {
+//   if (cityValue.value.length < 3) {
+//     console.log(`Pole niepoprawne`);
+//     cityValue.classList.add("red");
+//   } else {
+//     console.log(`Pole poprawne`);
+//     cityValue.classList.remove("red");
+//   }
+// });
 
-nameValue.addEventListener("input", function () {
-  if (nameValue.value.length < 3) {
-    console.log(`Pole niepoprawne`);
-    nameValue.classList.add("red");
-  } else {
-    console.log(`Pole poprawne`);
-    nameValue.classList.remove("red");
-  }
-});
-cityValue.addEventListener("input", function () {
-  if (cityValue.value.length < 3) {
-    console.log(`Pole niepoprawne`);
-    cityValue.classList.add("red");
-  } else {
-    console.log(`Pole poprawne`);
-    cityValue.classList.remove("red");
-  }
-});
+// codeValue.addEventListener("input", function () {
+//   if (!regExPostCode.test(codeValue.value)) {
+//     //dodać (codeValue.value.indexOf('-')!= 2)
+//     console.log(`Pole niepoprawne`);
+//     codeValue.classList.add("red");
+//   } else {
+//     console.log(`Pole poprawne`);
+//     codeValue.classList.remove("red");
+//   }
+// });
 
-const regExPostCode = /\d{2}-\d{3}/;
-
-codeValue.addEventListener("input", function () {
-  if (!regExPostCode.test(codeValue.value)) {
-    //dodać (codeValue.value.indexOf('-')!= 2)
-    console.log(`Pole niepoprawne`);
-    codeValue.classList.add("red");
-  } else {
-    console.log(`Pole poprawne`);
-    codeValue.classList.remove("red");
-  }
-});
-
-const regExEmail = /^\S+@\S+\.\S+$/;
-
-emailValue.addEventListener("input", function () {
-  if (!regExEmail.test(emailValue.value)) {
-    //dodać (codeValue.value.indexOf('-')!= 2)
-    console.log(`Pole niepoprawne`);
-    emailValue.classList.add("red");
-  } else {
-    console.log(`Pole poprawne`);
-    emailValue.classList.remove("red");
-  }
-});
+// emailValue.addEventListener("input", function () {
+//   if (!regExEmail.test(emailValue.value)) {
+//     //dodać (codeValue.value.indexOf('-')!= 2)
+//     console.log(`Pole niepoprawne`);
+//     emailValue.classList.add("red");
+//   } else {
+//     console.log(`Pole poprawne`);
+//     emailValue.classList.remove("red");
+//   }
+// });
 
 // Second method
 // for (let i = 0; i < 3; i++) {
@@ -88,21 +85,22 @@ emailValue.addEventListener("input", function () {
 //   });
 // }
 
-// //Third Method
-// const validateCondition = {
-//   0: (value) => value > 3,
-//   1: (value) => value > 3,
-//   2: (value) => value === 6,
-// };
+//Third Method
+const validateCondition = {
+  0: (value) => value.length > 3,
+  1: (value) => value.length > 3,
+  2: (value) => regExPostCode.test(value),
+  3: (value) => regExEmail.test(value),
+};
 
-// for (let i = 0; i < 3; i++) {
-//   form[i].addEventListener("input", function () {
-//     if (validateCondition[i](form[i].value.length)) {
-//       console.log(`Pole poprawne`);
-//       form[i].classList.remove("red");
-//     } else {
-//       console.log(`Pole niepoprawne`);
-//       form[i].classList.add("red");
-//     }
-//   });
-// }
+for (let i = 0; i < 4; i++) {
+  form[i].addEventListener("input", function () {
+    if (validateCondition[i](form[i].value)) {
+      console.log(`Pole poprawne`);
+      form[i].classList.remove("red");
+    } else {
+      console.log(`Pole niepoprawne`);
+      form[i].classList.add("red");
+    }
+  });
+}
